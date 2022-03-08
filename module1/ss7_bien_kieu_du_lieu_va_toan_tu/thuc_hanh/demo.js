d25501f1a2c6e5d7662df010c62daa2f1c3cfc8d
tunhutuyen <conlocdn@gmail.com>
2022-03-08 18:46:53 +0700

    nop bai tap ngay 8-3

---
+++ module1/ss7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/demo.js
@@ -0,0 +1,4 @@
+let inputWidth = Number(prompt("Nhập chiều rộng: "));
+let inputHeight = Number(prompt("Nhập chiều cao: "));
+let area = inputHeight*inputWidth;
+alert("Với width = "+ inputWidth +" "+ "và height = "+inputHeight+ " thì diện tích hình chữ nhật là "+ area);
