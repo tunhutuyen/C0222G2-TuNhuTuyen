f823ecfe664af084cc88ce5243cefbdc09414be2
tunhutuyen <conlocdn@gmail.com>
2022-04-12 21:17:39 +0700

    12-4

--- module2/src/ss7_abstract_class_and_interface/bai_tap/interface_colorable_for_class_shap/Main.java
+++ module2/src/ss7_abstract_class_and_interface/bai_tap/interface_colorable_for_class_shap/Main.java
@@ -10,12 +10,13 @@ public class Main {
         arrayShape[0] = new Circle();
         arrayShape[1] = new Square();
         arrayShape[2] = new Rectangle();
-        System.out.println(arrayShape[0] instanceof Circle);
+        System.out.println(arrayShape[1] instanceof Square);
+        System.out.println(arrayShape[1] instanceof Colorable);
         for (Shape shape:arrayShape) {
             if (shape instanceof Circle){
                 System.out.println("areaCircle = "+((Circle)shape).getArea());
             }
-            if (shape instanceof Square){
+            if (shape instanceof Colorable){
                 System.out.println("areaSquare = "+((Square)shape).getArea());
                 ((Square) shape).howToColor();;
             }
